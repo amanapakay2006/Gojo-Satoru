e17579c1482d682a3102450ef57ce6f4fe183f3e
amanapakay2006 <108470509+amanapakay2006@users.noreply.github.com>
2022-06-30 18:27:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94756943099']
+global.premium = ['94756943099']
+global.ownernomer = '94756943099'
+global.ownername = '꧁✰ₛͥₑͣₓ⃪ͫॐ⃞🍁𝚜𝚊𝚍𝚒𝚢𝚊꧂'
+global.botname = '꧁✰ₛͥₑͣₓ⃪ͫॐ⃞🍁𝚜𝚊𝚍𝚒𝚢𝚊꧂'
+global.footer = '꧁✰ₛͥₑͣₓ⃪ͫॐ⃞🍁𝚜𝚊𝚍𝚒𝚢𝚊꧂.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'sri lanka'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🐦','❣️','🌺']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
